9e44aaa3df27044a3e39ee6298815568bcd82e20
Cheng-Yi Tang <tangcy1002@gmail.com>
2025-02-01 01:39:34 -0800

    Day6: Matrix

---
+++ leetcode-top150/48_rotate_image.py
@@ -0,0 +1,28 @@
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        
+        n = len(matrix)
+
+        top = 0
+        bottom = n - 1
+
+        # vertical reversal
+        while top < bottom:
+            for col in range(n):
+                temp = matrix[top][col]
+                matrix[top][col] = matrix[bottom][col]
+                matrix[bottom][col] = temp
+            top += 1
+            bottom -= 1
+
+        # Transpose
+        for row in range(n):
+            for col in range(row+1, n):
+                temp = matrix[row][col]
+                matrix[row][col] = matrix[col][row]
+                matrix[col][row] = temp
+
+        return matrix
